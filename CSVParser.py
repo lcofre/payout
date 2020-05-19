abaeefb636c7c210171c75f5c8f8453d344def92
Leandro Cofre <leandro.cofre@advantech.com>
2020-05-19 18:16:32 +0100

    Generic CSV parser created

---
+++ CSVParser.py
@@ -0,0 +1,21 @@
+import csv
+
+
+class CSVParser:
+    def __init__(self, file_name):
+        self.input_file = open(file_name, 'r')
+        self.csv_file = csv.DictReader(self.input_file)
+
+    def next_line(self):
+        next_line = next(self.csv_file, None)
+        if not next_line:
+            self.input_file.close()
+        return next_line
+
+    def all_as_list(self):
+        all_rows = []
+        current_line = self.next_line()
+        while current_line:
+            all_rows.append(current_line)
+            current_line = self.next_line()
+        return all_rows
